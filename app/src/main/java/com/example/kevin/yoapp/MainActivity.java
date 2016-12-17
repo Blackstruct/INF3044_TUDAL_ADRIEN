7765e66dccd9f6bb51d4a5605c98da3282299452
tudal kevin <tudal kevin>
2016-12-17 23:02:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kevin/yoapp/MainActivity.java
@@ -0,0 +1,178 @@
+package com.example.kevin.yoapp;
+
+import android.app.DatePickerDialog;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG="MainAct";
+    private DatePickerDialog dpd=null;
+    public   RecyclerView rvi=null;
+    public static final String BIERS_UPDATE="com.example.kevin.MainActivity.BIERS_UPDATE";
+    public class BierUpdate extends BroadcastReceiver{
+        public void onReceive(Context context, Intent intent){
+            notificatoin_test();
+            BiersAdapter a=(BiersAdapter)rvi.getAdapter();
+            a.setNewBiere();
+        }
+    }
+    private class BiersAdapter extends RecyclerView.Adapter<BiersAdapter.BierHolder>{
+
+        private JSONArray biere=null;
+
+        public BiersAdapter(JSONArray beer){
+            this.biere=beer;
+        }
+
+        public void setNewBiere(){
+            this.biere=getBiersFromFile();
+            notifyDataSetChanged();
+            Log.d(TAG, String.valueOf(getItemCount()));
+        }
+
+        @Override
+        public BierHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater lf=LayoutInflater.from(parent.getContext());
+            View vvvvvvvvvvvvv=lf.inflate(R.layout.rv_biere_element,parent,false);
+            BierHolder bieeeeeeer=new BierHolder(vvvvvvvvvvvvv);
+            return bieeeeeeer;
+        }
+
+        @Override
+        public void onBindViewHolder(BierHolder holder, int position) {
+            try {
+                JSONObject js=biere.getJSONObject(position);
+                String nom=js.getString("name");
+                holder.name.setText(nom);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return biere.length();
+        }
+
+        public class BierHolder extends RecyclerView.ViewHolder{
+            public  TextView name;
+            public BierHolder(View itemView) {
+                super(itemView);
+                name=(TextView)itemView.findViewById(R.id.rv_biere_element_name);
+            }
+        }
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button btn_test=(Button)findViewById(R.id.button);
+        DatePickerDialog.OnDateSetListener odsl=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                btn_test.setText(dayOfMonth+"/"+month+"/"+year);
+            }
+        };
+        dpd=new DatePickerDialog(this, odsl, 2000, 10, 20);
+        IntentFilter intentFilter=new IntentFilter(BIERS_UPDATE);
+        LocalBroadcastManager.getInstance(this).registerReceiver(new BierUpdate(),intentFilter);
+        rvi=(RecyclerView)findViewById(R.id.rv_biere);
+        rvi.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        rvi.setAdapter(new BiersAdapter(getBiersFromFile()));
+    }
+
+    public void notificatoin_test(){
+        NotificationCompat.Builder po= new NotificationCompat.Builder(this);
+        po.setSmallIcon(R.mipmap.ic_launcher).setContentTitle("yolo").setContentText("telechargement fini");
+
+        NotificationManager pl=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        pl.notify(153, po.build());
+    }
+
+    public void btn_press(View v){
+       // notificatoin_test();
+        //dpd.show();
+        if(rvi.getVisibility()==View.INVISIBLE) {
+            Yoservice.startActionFoo(this);
+            rvi.setVisibility(View.VISIBLE);
+            Toast.makeText(getApplicationContext(),getString(R.string.beer),Toast.LENGTH_LONG).show();
+        }else{
+            rvi.setVisibility(View.INVISIBLE);
+            Toast.makeText(getApplicationContext(),getString(R.string.disbeer),Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void frap_touch(View v){
+        String mot[]=new String[4];
+        mot[0]=getString(R.string.frap0);
+        mot[1]=getString(R.string.frap1);
+        mot[2]=getString(R.string.frap2);
+        mot[3]=getString(R.string.frap3);
+        Random rand=new Random();
+        Toast.makeText(getApplicationContext(),mot[rand.nextInt(4)],Toast.LENGTH_LONG).show();
+    }
+
+    public void press_toast(View v){
+        Toast.makeText(getApplicationContext(),getString(R.string.msg),Toast.LENGTH_LONG).show();
+    }
+
+    public void press_poke(View v){
+        Toast.makeText(getApplicationContext(),getString(R.string.poke),Toast.LENGTH_LONG).show();
+        startActivity(new Intent(this,pokemontrue.class));
+    }
+
+    public void press_map(View v){
+        Toast.makeText(getApplicationContext(),getString(R.string.voyage),Toast.LENGTH_LONG).show();
+        startActivity(new Intent(this, Main2Activity.class));
+    }
+
+    public JSONArray getBiersFromFile(){
+        try{
+            InputStream is=new FileInputStream(getCacheDir()+"/"+"biere.json");
+            byte[] buffer=new byte[is.available()];
+            is.read(buffer);
+            is.close();
+            return new JSONArray(new String(buffer,"UTF-8"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return new JSONArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new JSONArray();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return new JSONArray();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/kevin/yoapp/Yoservice.java
@@ -0,0 +1,98 @@
+package com.example.kevin.yoapp;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.util.StringBuilderPrinter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class Yoservice extends IntentService {
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_FOO = "com.example.kevin.yoapp.action.FOO";
+    private static final String TAG="Yoservice";
+    public Yoservice() {
+        super("Yoservice");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionFoo(Context context) {
+        Intent intent = new Intent(context, Yoservice.class);
+        intent.setAction(ACTION_FOO);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_FOO.equals(action)) {
+                handleActionFoo();
+                LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(MainActivity.BIERS_UPDATE));
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void copyInputStreamToFile(InputStream in,File file){
+        try{
+            OutputStream out=new FileOutputStream(file);
+            byte[] buf=new byte[1024];
+            int len;
+            while((len=in.read(buf))>0){
+                out.write(buf,0,len);
+            }
+            out.close();
+            in.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void handleActionFoo() {
+        Log.d(TAG,Thread.currentThread().getName());
+        URL url=null;
+        try{
+            url=new URL("http://binouze.fabrigli.fr/bieres.json");
+            HttpURLConnection conn=(HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            if(HttpURLConnection.HTTP_OK==conn.getResponseCode()){
+                copyInputStreamToFile(conn.getInputStream(),new File(getCacheDir(),"biere.json"));
+                Log.d(TAG,"ok");
+            }
+        }catch(MalformedURLException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
